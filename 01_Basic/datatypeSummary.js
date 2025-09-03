bbf3e6f5e9b9b449c8cf581fa6e2bd68bdee43f3
Gulshan Kumari <gulshan.kumari2207@gmail.com>
2025-09-03 09:10:01 +0000

    datatype summary

---
+++ 01_Basic/datatypeSummary.js
@@ -0,0 +1,18 @@
+const outsideTemp = null
+let userEmail;
+const id =Symbol('123')
+const anotherId = Symbol('123')
+// Both are not same.
+// console.log(id === anotherId)
+
+const bigNumber=67453747478744638766836n
+const heros =["jfdjk", "djhius","djhdh"]; 
+let myObj = {
+    name : "Gulshan",
+    age: 23,
+}
+const myFunction =function(){
+    console.log("Hello, I am learning js");
+}
+console.log(typeof bigNumber)
+console.log(typeof anotherId)
