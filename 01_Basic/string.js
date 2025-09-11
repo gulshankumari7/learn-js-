a373791bd6103845cbf27131129e26d0958b642b
Gulshan Kumari <gulshan.kumari2207@gmail.com>
2025-09-11 00:28:10 +0000

    Basic String

--- 01_Basic/string.js
+++ 01_Basic/string.js
@@ -2,9 +2,9 @@ const name = "Divya"
 const repo = 89
 // console.log(name +" " +repo +" " +"value")
 // String interpolation
-// console.log(`Hello this is ${name} and my repo ${repo}`);
+console.log(`Hello this is ${name} and my repo ${repo}`);
 // String is an object
-const gameName = new String('Gulshan Kumari')
+const gameName = new String('Gulshan-Kumari')
 console.log(gameName[0])
 console.log(gameName[1])
 console.log(gameName[2])
@@ -20,3 +20,14 @@ console.log(gameName.length)
 console.log(gameName.charAt(2))
 // find position.
 console.log(gameName.indexOf('t'))
+const newString = gameName.substring(0,4)
+console.log(newString)
+const anotherString = gameName.slice(-14,3)
+console.log(anotherString)
+const newString1 ="   Gulshan   "
+console.log(newString1)
+console.log(newString1.trim())//trim remove spaces.
+const url="https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'))
+console.log(url.includes('divya'))
+console.log(gameName.split('_'))
