e95ebb2c3b0b0384b4f13b4b657c3ceca5b806bb
Gulshan Kumari <gulshan.kumari2207@gmail.com>
2025-10-15 02:07:15 +0000

    object symbol and know how to use string interpolation

--- 02_Basic/objects.js
+++ 02_Basic/objects.js
@@ -1,9 +1,10 @@
 
 const mySym = Symbol("key1")
-
+//  To access object value using dot notation.
 const jsUser = {
     name : " Gulshan Kumari",
-    "full_name" : "Divya Singh",    
+    "full_name" : "Divya Singh",  
+    // [mySym] using [] is a symbol key syntax. 
     [mySym] : "MyKEY1",
     age : 22,
     location :  "Bihar",
@@ -11,7 +12,25 @@ const jsUser = {
     isLoggedIn : false,
     lastLoginDays : ["Monday", "Saturday"]
 }
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser["full_name"]);
-console.log(jsUser[mySym]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full_name"]);
+// console.log(jsUser[mySym]);
+
+jsUser.email = " gulshan2207@gmail.com"
+// Object.freeze(jsUser)
+jsUser.email = " gulshangpt@gmail.com"
+// console.log(jsUser);
+
+// use functions 
+
+jsUser.greeting = function(){
+    console.log("Hello js users");
+}
+console.log(jsUser.greeting());
+
+// useing string interpolation
+jsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}`);
+}
+console.log(jsUser.greetingTwo());
